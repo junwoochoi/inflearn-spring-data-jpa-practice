ad2d4fe337a503f30d8da81a36652e5123d12a40
junwoochoi <cjw4690@gmail.com>
2020-02-20 22:16:33 +0900

    test : test QueryDSL repository

--- src/main/java/com/ingang/datajpa/repository/MemberRepositoryImpl.java
+++ src/main/java/com/ingang/datajpa/repository/MemberRepositoryImpl.java
@@ -1,20 +1,29 @@
 package com.ingang.datajpa.repository;
 
 import com.ingang.datajpa.entity.Member;
+import com.ingang.datajpa.entity.QMember;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+import static com.ingang.datajpa.entity.QMember.*;
+
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    public MemberRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
 
     @Override
     public List<Member> findMemberCustom() {
-        return em.createQuery("select m from Member m order by m.username desc", Member.class)
-                .getResultList();
+        return queryFactory.
+                selectFrom(member)
+                .orderBy(member.username.desc())
+                .fetch();
     }
 
 }
